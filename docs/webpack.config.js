83176ed07daff99e9b58e49d514617406e351930
Atirek <atirek.kumar@couture.ai>
2021-07-19 04:12:18 +0530

    chore: renaming dev webpack.config.js

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -323,20 +323,20 @@ const config = {
         // include source code for plugins, but exclude node_modules and test files within them
         exclude: [
           /superset-ui.*\/node_modules\//,
-          /superset-viz-plugins.*\/node_modules\//,
+          /improved-octo-succotash.*\/node_modules\//,
           /\.test.jsx?$/,
         ],
         include: [
           new RegExp(`${APP_DIR}/src`),
           /superset-ui.*\/src/,
-          /superset-viz-plugins.*\/src/,
+          /improved-octo-succotash.*\/src/,
           new RegExp(`${APP_DIR}/.storybook`),
         ],
         use: [babelLoader],
       },
       {
         test: /\.css$/,
-        include: [APP_DIR, /superset-ui.+\/src/, /superset-viz-plugins.+\/src/],
+        include: [APP_DIR, /superset-ui.+\/src/, /improved-octo-succotash.+\/src/],
         use: [
           isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
           {
@@ -446,7 +446,7 @@ if (isDevMode) {
   Object.entries(packageConfig.dependencies).forEach(([pkg, version]) => {
     const srcPath = `./node_modules/${pkg}/src`;
     if (
-      (/superset-ui/.test(pkg) || /superset-viz-plugins/.test(pkg)) &&
+      (/superset-ui/.test(pkg) || /improved-octo-succotash/.test(pkg)) &&
       fs.existsSync(srcPath)
     ) {
       console.log(
